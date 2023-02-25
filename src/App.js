31b383b8e566e4072dde3ebf7051305d044584c3
Roman <kurpelroma@gmail.com>
2023-02-25 14:19:58 +0200

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import './App.css';
 import Nav from './Components/Nav/Nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProfileContainer from './Components/Profile/Profile-Container';
 import MessagesContainer from './Components/Messages/Messages-Container';
-import UsersContainer from './Components/Users/Users-Container';
 import HeaderContainer from './Components/Header/Header-Container';
 import Login from './Components/Login/Login';
 import { connect } from 'react-redux';
 import { initalApp } from './Components/Redux/app-reducer';
 import Preloader from './Components/common/Preloader';
+const HeavyComponent = lazy(() => import('./Components/Users/Users-Container'));
 class App extends React.Component {
   componentDidMount() {
     this.props.initalApp ()
@@ -23,10 +23,12 @@ class App extends React.Component {
             <HeaderContainer/>
             <Nav data={this.props.data.sideBarPage}/>
             <Routes>
-              <Route path='/Profile/:userId?' element={<ProfileContainer />} />
-              <Route path='/Messages/*' element={<MessagesContainer/>} />
-              <Route path='/Users' element={<UsersContainer />} />
-              <Route path='/login' element={<Login/>} />
+              <Route path='/Profile/:userId?' basename={process.env.PUBLIC_URL} element={<ProfileContainer />} />
+              <Route path='/Messages/*' basename={process.env.PUBLIC_URL} element={<MessagesContainer/>} />
+              <Route path='/Users' basename={process.env.PUBLIC_URL} element={<Suspense fallback={<div>Loading...</div>}>
+                                              <HeavyComponent />
+                                            </Suspense>} />
+              <Route path='/login' basename={process.env.PUBLIC_URL} element={<Login/>} />
             </Routes>
           </div>
         </BrowserRouter>
