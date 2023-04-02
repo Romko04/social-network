8f4d9f8cbb8f2db62dbf4d29300230eedb8c2255
Roman <kurpelroma@gmail.com>
2023-04-02 10:17:19 +0300

    Пофіксив баг

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -6,17 +6,20 @@ import {ReactComponent as Logout} from '../../icons/logout.svg'
 import { NavLink } from 'react-router-dom'
 import './Nav.css'
 import { setUserTypeData } from 'types/types'
+import { useSelector } from 'react-redux'
+import { appStateType } from 'Components/Redux/redux-store'
 export type navPropsType = {
     logoutThunk:()=>void,
     auth: setUserTypeData
 }
 const Nav:React.FC<navPropsType> = ({logoutThunk,auth}) => {
+    const isAuth = useSelector((state: appStateType) => state.auth.isAuth)
     return (
         <nav className='nav'>
             <NavLink className={({ isActive }) => isActive ? 'active nav__link' : 'nav__link'} to="/Profile">
                 <Profile className='profile__icon'/> <span className="profile__nav-link-value">Profile</span>
             </NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'active nav__link' : 'nav__link'} to="/Messages">
+            <NavLink className={({ isActive }) => isActive && isAuth? 'active nav__link' : 'nav__link'} to="/Messages">
                 <Messages className='profile__icon'/> <span className="profile__nav-link-value">Messages</span>
             </NavLink>
             <NavLink className={({ isActive }) => isActive ? 'active nav__link' : 'nav__link'} to="/Users">
